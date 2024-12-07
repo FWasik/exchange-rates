18e86ef67c5fe54e24ebad8153b0d79b48ddacae
FWasik <franekwasik2@gmail.com>
2024-12-07 23:48:13 +0100

    feat: rates app introduce + dependencies

--- exchange_rates/exchange_rates/settings.py
+++ exchange_rates/exchange_rates/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    "rates"
 ]
 
 MIDDLEWARE = [
@@ -108,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
@@ -124,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False,
+}
---
+++ exchange_rates/rates/views.py
@@ -0,0 +1,23 @@
+from rest_framework import views, status, generics
+from rest_framework.response import Response
+from .models import Currency, ExchangeRate
+from .serializers import CurrencySerializer, ExchangeRateSerializer
+
+
+class CurrencyListView(generics.ListAPIView):
+    queryset = Currency.objects.all().order_by("code")
+    serializer_class = CurrencySerializer
+
+
+class ExchangeRateDetailView(views.APIView):
+    def get(self, request, base_currency, target_currency):
+        pair = base_currency + target_currency
+        
+        try:
+            latest_rate = ExchangeRate.objects.filter(currency_pair=pair).latest("timestamp")
+            serializer = ExchangeRateSerializer(latest_rate)
+            
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        except ExchangeRate.DoesNotExist:
+            return Response({"detail": f"Exchange rate for the given currency pair {pair} does not exist."}, status=status.HTTP_404_NOT_FOUND) 
